cb41a61c97d3bdbe575018f25024ed6a41888c58
jazibsalman123 <jazib1341@gmail.com>
2024-10-20 22:11:47 +0500

    jazib

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,11 @@ Route::view("/index","admin.pages.index");
 
 Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
