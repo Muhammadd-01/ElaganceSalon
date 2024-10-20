562ff41e401ed2abc509164212a1239a07ec89ef
Muhammad Affan <muhammadaffan1445@gmail.com>
2024-10-20 19:18:45 +0500

    changes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Auth;
 
+
+//User Routes Start
+
 Route::get('/', function () {
     return view('users.pages.welcome');
 });
@@ -23,9 +26,25 @@ Route::view("/blogGrid","users.pages.blog-grid");
 Route::view("/blogClassic","users.pages.blog-classic");
 Route::view("/appointment","users.pages.appointment");
 Route::view("/aboutCompany","users.pages.about-company");
-Route::view("/404","users.pages.404");
-Route::view("/templateAdmin","admin.pages.template");
-Route::view("/index","admin.pages.index");
+Route::view("/user404","users.pages.404");
+
+//User Routes End
+
+//Admin Routes Start
+Route::view("/dash","admin.dashboard");
+Route::view("/404","admin.404");
+Route::view("/blank","admin.blank");
+Route::view("/button","admin.button");
+Route::view("/chart","admin.chart");
+Route::view("/element","admin.element");
+Route::view("/form","admin.form");
+Route::view("/adminSignIn","admin.signin");
+Route::view("/adminSignUp","admin.signup");
+Route::view("/table","admin.table");
+Route::view("/spin","admin.spinTem");
+Route::view("/index","admin.index");
+Route::view("/typo","admin.typography");
+Route::view("/widget","admin.widget");
 
 Auth::routes();
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
