a25bdde01edb988466889c54397b8fc53e7c6c89
TheBossMan110 <syedzakihaider2006@gmail.com>
2024-11-05 20:35:30 -0800

    zaki

---
+++ resources/views/users/pages/appointment.blade.php
@@ -0,0 +1,232 @@
+@extends('users.pages.template')
+
+@section('content')
+<section class="page_header d-flex align-items-center">
+    <div class="container">
+        <div class="section_heading text-center mb-40 wow fadeInUp" data-wow-delay="300ms">
+            <h3>Trendy Salon &amp; Spa</h3>
+            <h2>Appointment</h2>
+            <div class="heading-line"></div>
+        </div>
+    </div>
+</section>
+<section class="service_section bg-grey padding">
+    <div class="container">
+        <div class="section_heading text-center mb-40 wow fadeInUp" data-wow-delay="300ms">
+            <h3>Trendy Salon &amp; Spa</h3>
+            <h2>Our Services</h2>
+            <div class="heading-line"></div>
+        </div>
+        <div class="row">
+            <div class="col-lg-3 col-md-6 sm-padding wow fadeInUp" data-wow-delay="200ms">
+                <div class="service_box">
+                    <i class="bs bs-scissors-1"></i>
+                    <h3>Haircut Styles</h3>
+                    <p>Barber is a person whose occupation is mainly to cut dress style.</p>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 sm-padding wow fadeInUp" data-wow-delay="300ms">
+                <div class="service_box">
+                    <i class="bs bs-razor-2"></i>
+                    <h3>Beard Trimming</h3>
+                    <p>Barber is a person whose occupation is mainly to cut dress style.</p>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 sm-padding wow fadeInUp" data-wow-delay="400ms">
+                <div class="service_box">
+                    <i class="bs bs-brush"></i>
+                    <h3>Smooth Shave</h3>
+                    <p>Barber is a person whose occupation is mainly to cut dress style.</p>
+                </div>
+            </div>
+            <div class="col-lg-3 col-md-6 sm-padding wow fadeInUp" data-wow-delay="500ms">
+                <div class="service_box">
+                    <i class="bs bs-hairbrush-1"></i>
+                    <h3>Face Masking</h3>
+                    <p>Barber is a person whose occupation is mainly to cut dress style.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<section class="book_section padding">
+    <div class="book_bg"></div>
+    <div class="map_pattern"></div>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6 offset-md-6">
+                <form action="https://html.dynamiclayers.net/dl/barbershop/appointment.php" method="post"
+                    id="appointment_form" class="form-horizontal appointment_form">
+                    <div class="book_content">
+                        <h2>Make an appointment</h2>
+                        <p>Barber is a person whose occupation is mainly to cut dress groom <br>style and shave
+                            men's and boys hair.</p>
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-md-6 padding-10">
+                            <input type="text" id="app_name" name="app_name" class="form-control" placeholder="Name"
+                                required>
+                        </div>
+                        <div class="col-md-6 padding-10">
+                            <input type="email" id="app_email" name="app_email" class="form-control"
+                                placeholder="Your Email" required>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-md-6 padding-10">
+                            <input type="text" id="app_phone" name="app_phone" class="form-control"
+                                placeholder="Your Phone No" required>
+                        </div>
+                        <div class="col-md-6 padding-10">
+                            <input type="text" id="app_free_time" name="app_free_time" class="form-control"
+                                placeholder="Your Free Time" required>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-md-6 padding-10">
+                            <select class="form-control" id="app_services" name="app_services" onchange="updatePrice()">
+                                <option value="">Select Service</option>
+                                <option value="hair_styling">Hair Styling</option>
+                                <option value="shaving">Shaving</option>
+                                <option value="face_mask">Face Mask</option>
+                                <option value="hair_wash">Hair Wash</option>
+                                <option value="beard_trimming">Beard Trimming</option>
+                                <option value="message_therapy">Message Therapy</option>
+                            </select>
+                        </div>
+                        <div class="col-md-6 padding-10">
+                            <select class="form-control" id="app_barbers" name="app_barbers">
+                                <option>Choose Barbers</option>
+                                <option>Michel Brown</option>
+                                <option>Jonathan Smith</option>
+                                <option>Jack Tosan</option>
+                                <option>Martin Lane</option>
+                            </select>
+                        </div>
+                    </div>
+                    <div class="form-group row">
+                        <div class="col-md-12 padding-10">
+                            <p id="service_price" class="text-center" style="font-weight: bold;"></p>
+                        </div>
+                    </div>
+                    <button id="app_submit" class="default_btn" type="submit">Make Appointment</button>
+                    <div id="msg-status" class="alert" role="alert"></div>
+                </form>
+            </div>
+        </div>
+    </div>
+</section>
+<section id="team" class="team_section bd-bottom padding">
+    <div class="container">
+        <div class="section_heading text-center mb-40 wow fadeInUp" data-wow-delay="300ms">
+            <h3>Trendy Salon &amp; Spa</h3>
+            <h2>Our Barbers</h2>
+            <div class="heading-line"></div>
+        </div>
+        <ul class="team_members row">
+            <li class="col-lg-3 col-md-6 sm-padding wow fadeInUp" data-wow-delay="200ms">
+                <div class="team_member">
+                    <img src="img/team-1.jpg" alt="Team Member">
+                    <div class="overlay">
+                        <h3>Kyle Frederick</h3>
+                        <p>WEB DESIGNER</p>
+                    </div>
+                </div>
+            </li>
+            <li class="col-lg-3 col-md-6 sm-padding wow fadeInUp" data-wow-delay="300ms">
+                <div class="team_member">
+                    <img src="img/team-2.jpg" alt="Team Member">
+                    <div class="overlay">
+                        <h3>José Carpio</h3>
+                        <p>WORDPRESS DEVELOPER</p>
+                    </div>
+                </div>
+            </li>
+            <li class="col-lg-3 col-md-6 sm-padding wow fadeInUp" data-wow-delay="400ms">
+                <div class="team_member">
+                    <img src="img/team-3.jpg" alt="Team Member">
+                    <div class="overlay">
+                        <h3>Michel Ibáñez</h3>
+                        <p>ONLINE MARKETER</p>
+                    </div>
+                </div>
+            </li>
+            <li class="col-lg-3 col-md-6 sm-padding wow fadeInUp" data-wow-delay="500ms">
+                <div class="team_member">
+                    <img src="img/team-4.jpg" alt="Team Member">
+                    <div class="overlay">
+                        <h3>Adam Castellon</h3>
+                        <p>JAVA SPECIALIST</p>
+                    </div>
+                </div>
+            </li>
+        </ul>
+    </div>
+</section>
+<section class="cta_section padding">
+    <div class="container">
+        <div class="display-table">
+            <div class="table-cel">
+                <div class="cta_content align-center wow fadeInUp" data-wow-delay="300ms">
+                    <h2>Making You Look Good <br> Is In Our Heritage.</h2>
+                    <p>Barber is a person whose occupation is mainly to cut dress groom <br>style and shave men's
+                        and boys hair.</p>
+                    <a href="#" class="default_btn">Make Appointment</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+<div class="sponsor_section bg-grey padding">
+    <div class="container">
+        <ul id="sponsor_carousel" class="sponsor_items owl-carousel">
+            <li class="sponsor_item">
+                <img src="img/sponsor-1.png" alt="sponsor-image">
+            </li>
+            <li class="sponsor_item">
+                <img src="img/sponsor-2.png" alt="sponsor-image">
+            </li>
+            <li class="sponsor_item">
+                <img src="img/sponsor-3.png" alt="sponsor-image">
+            </li>
+            <li class="sponsor_item">
+                <img src="img/sponsor-4.png" alt="sponsor-image">
+            </li>
+            <li class="sponsor_item">
+                <img src="img/sponsor-5.png" alt="sponsor-image">
+            </li>
+            <li class="sponsor_item">
+                <img src="img/sponsor-1.png" alt="sponsor-image">
+            </li>
+            <li class="sponsor_item">
+                <img src="img/sponsor-2.png" alt="sponsor-image">
+            </li>
+            <li class="sponsor_item">
+                <img src="img/sponsor-3.png" alt="sponsor-image">
+            </li>
+        </ul>
+    </div>
+</div>
+<script>
+    function updatePrice() {
+        const prices = {
+            hair_styling: "$30",
+            shaving: "$20",
+            face_mask: "$25",
+            hair_wash: "$15",
+            beard_trimming: "$18",
+            message_therapy: "$40"
+        };
+
+        const serviceSelect = document.getElementById('app_services');
+        const selectedService = serviceSelect.value;
+        const priceDisplay = document.getElementById('service_price');
+
+        if (prices[selectedService]) {
+            priceDisplay.textContent = `Price: ${prices[selectedService]}`;
+        } else {
+            priceDisplay.textContent = '';
+        }
+    }
+</script>
+@endsection
